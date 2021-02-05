db751ed8d3d1101a5b4b64b31a1f3ee17d244302
gxmari007 <392041299@qq.com>
2021-02-05 15:39:13 +0800

    feat: complete command code

--- index.js
+++ index.js
@@ -8,6 +8,47 @@ const pkg = require('./package.json');
 
 const cwd = process.cwd();
 const templates = ['lib-ts'];
+const renameFiles = {
+  _gitignore: '.gitignore',
+};
+
+function copy(src, dest) {
+  const stat = fs.statSync(src);
+
+  if (stat.isDirectory()) {
+    copyDir(src, dest);
+  } else {
+    fs.copyFileSync(src, dest);
+  }
+}
+
+function copyDir(src, dest) {
+  fs.mkdirSync(dest, { recursive: true });
+
+  for (const file of fs.readdirSync(src)) {
+    const srcFile = path.resolve(src, file);
+    const targetFile = path.resolve(dest, file);
+
+    copy(srcFile, targetFile);
+  }
+}
+
+function emptyDir(dir) {
+  if (!fs.existsSync(dir)) {
+    return;
+  }
+
+  for (const file of fs.readdirSync(dir)) {
+    const abs = path.resolve(dir, file);
+
+    if (fs.lstatSync(abs).isDirectory()) {
+      emptyDir(abs);
+      fs.rmdirSync(abs);
+    } else {
+      fs.unlinkSync(abs);
+    }
+  }
+}
 
 program.version(pkg.version, '-v, --version', 'output the version number');
 program.helpOption('-h, --help', 'output usage information');
@@ -32,7 +73,6 @@ program
     }
 
     const root = path.join(cwd, projectDir);
-    console.log(root);
 
     // determine template
     let template = options.template;
@@ -48,10 +88,58 @@ program
       template = t;
     }
 
-    const templateDir = path.join(cwd, 'templates', `template-${template}`);
+    const templateDir = path.join(__dirname, 'templates', `template-${template}`);
     const files = fs.readdirSync(templateDir);
 
-    console.log(files);
+    const write = (file, content) => {
+      const targetPath = renameFiles[file]
+        ? path.join(root, renameFiles[file])
+        : path.join(root, file);
+
+      if (content) {
+        fs.writeFileSync(targetPath, content);
+      } else {
+        copy(path.join(templateDir, file), targetPath);
+      }
+    };
+
+    if (!fs.existsSync(root)) {
+      fs.mkdirSync(root, { recursive: true });
+    } else {
+      const existing = fs.readdirSync(root);
+
+      if (existing.length) {
+        const { yes } = await prompt({
+          type: 'confirm',
+          name: 'yes',
+          initial: 'Y',
+          message: `
+            Target directory ${targetDir} is not empty.
+            Remove existing files and continue?
+          `
+        });
+
+        if (yes) {
+          emptyDir(root);
+        } else {
+          // exit program
+          return;
+        }
+      }
+    }
+
+    for (const file of files) {
+      if (file === 'package.json') {
+        const package = require(path.join(templateDir, 'package.json'));
+
+        package.name = path.basename(root);
+        write(file, JSON.stringify(package, null, 2));
+      } else {
+        write(file);
+      }
+    }
+
+    console.log('\nDone.');
   });
 
 program.parse(process.argv);
