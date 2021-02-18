9c4a779cd0ff602491a1eedb82cc4d9eb21e16b7
gxmari007 <392041299@qq.com>
2021-02-18 18:01:33 +0800

    feat: add ui template

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { prompt } from 'enquirer';
 import pkg from '../package.json';
 
 const cwd = process.cwd();
-const templates = ['lib-ts', 'vite-plugin'];
+const templates = ['components', 'lib-ts', 'vite-plugin'];
 const renameFiles = {
   _gitignore: '.gitignore',
   '_package.json': 'package.json',
